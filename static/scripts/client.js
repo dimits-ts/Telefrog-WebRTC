adb8dc7d8b8547e4802b9292cbc7b5a08e6bcae0
Dimitris Tsirbas <manisfatsa@gmail.com>
2022-11-17 13:20:03 +0200

    Build skeleton for receiving messages

--- static/scripts/client.js
+++ static/scripts/client.js
@@ -1,11 +1,12 @@
 // Configurations
 const URL = "http://localhost:8080"
 const streamConstraints = { audio: true, video: true };
+const CHAT_REFRESH_MS = 500;
 
 const socket = io(URL);
+const connectedPeers = {};
 
-const connectedPeers = {}
-
+// elements
 const loginPanel = document.getElementById("login");
 const roomBanner = document.getElementById("roomBanner");
 const videoPanel = document.getElementById("videoPanel");
@@ -23,8 +24,57 @@ const sendMessageButton = document.getElementById("sendMessage");
 const joinRoomButton = document.getElementById("join_room_button");
 const createRoomButton = document.getElementById("create_room_button");
 
+// globals
+const chat = new Chat();
 let roomId;
 let username;
+
+/**
+ *  A class containing the chat messages and displaying new ones to
+ *  the HTML chatbox.
+ */ 
+class Chat {
+    static NO_MESSAGES_ID = "";
+    #messages = []
+
+    getLastMessageId() {
+        if (this.messages.length == 0) {
+            return NO_MESSAGES_ID;
+        } else {
+            return this.messages[this.messages.length - 1];
+        }
+    }
+
+    addMessage(message) {
+        let username = message.username;
+        let type = message.type;
+
+        this.messages.append(message);
+
+        if(type === "Text"){
+            this.#addTextToChat(username, message.content);
+        } else if (type === "Image") {
+            this.#addImageToChat(username, message);
+        } else if (type === "File") {
+            this.#addFileToChat(username, message);
+        } else {
+            throw("Invalid message type " + type);
+        }
+    }
+
+    #addTextToChat(username, text) {
+        console.log(username, text);
+    }
+    
+    #addImageToChat(username, image) {
+        console.log(username, image);
+    }
+    
+    #addFileToChat(username, file) {
+        console.log(username, file);
+    }
+}
+
 
 // ========== CALL HANDLERS ==========
 
@@ -38,7 +88,7 @@ joinRoomButton.onclick = e => {
     const myVideo = document.createElement("video");
     myVideo.muted = true;
 
-    if (usernameIsValid(username)) 
+    if (usernameIsValid(username))
         showError("Please enter a valid username.");
     else
         //begin streaming
@@ -62,10 +112,55 @@ createRoomButton.onclick = e => {
 socket.on("user-disconnected", id => {
     console.log("User disconnected " + id);
 
-    if (connectedPeers[id]) 
+    if (connectedPeers[id])
         connectedPeers[id].close();
 });
 
+// ========== CHAT HANDLERS ==========
+
+sendMessageButton.addEventListener("click", () => {
+    // Send any field that is filled
+    text = chatInput.value;
+    if (text.trim() !== "")
+        sendText(text);
+
+
+    for (image of imageInput.files)
+        if (image)
+            sendImage(image);
+
+    for (file of fileInput.files)
+        if (file)
+            sendFile(file)
+
+    // reset input
+    chatInput.value = "";
+    imageInput.value = "";
+    fileInput.value = "";
+});
+
+// perioducally refresh chat showing new messages
+timedLoop(CHAT_REFRESH_MS, refreshChat);
+
+
+function refreshChat() {
+    console.log("Refreshing chat");
+
+    const data = new FormData();
+    data.append("room_id", roomId);
+    data.append("last_message", chat.getLastMessageId());
+
+    fetch(URL + "/chat-box/refresh", {
+        method: "GET",
+        body: data
+    }).then(res => res.json())
+        .then(list => {
+            // if no new messages nothing will happen
+            for (message of list) {
+                chat.addMessage(message);
+            }
+        });
+}
 
 function usernameIsValid(username) {
     // check if is whitespace
@@ -146,7 +241,7 @@ function addVideoStream(video, stream) {
     video.srcObject = stream;
     video.addEventListener("loadedmetadata", () => {
         video.play();
-    })
+    });
     videoGrid.append(video);
 }
 
@@ -169,30 +264,6 @@ function connectToNewUser(myPeer, userId, stream) {
     connectedPeers[userId] = call;
 }
 
-// ========== CHAT HANDLERS ==========
-
-sendMessageButton.addEventListener("click", () => {
-    // Send any field that is filled
-    text = chatInput.value;
-    if(text.trim() !== "") 
-        sendText(text);
-
-    
-    for(image of imageInput.files)
-        if(image)
-            sendImage(image);
-
-    for(file of fileInput.files)
-        if(file)
-            sendFile(file)
-
-    // reset input
-    chatInput.value = "";
-    imageInput.value = "";
-    fileInput.value = "";
-});
-
-
 function sendText(text) {
     const data = new FormData();
     data.append("room_id", roomId);
@@ -200,13 +271,12 @@ function sendText(text) {
     data.append("message_type", "Text");
     data.append("content", text);
 
-    fetch(URL + "/message/new", { 
-        method: "POST", 
-        body: data})
-        .then(res => res.json())
-        .then(response => {
-            // TODO: React to response
-        });
+    fetch(URL + "/message/new", {
+        method: "POST",
+        body: data
+    }).then(response => {
+        // TODO: React to response
+    });
     console.log("Sent text : " + text);
 }
 
@@ -218,13 +288,12 @@ function sendImage(image) {
     data.append("message_type", "Image");
     data.append("content", JSON.stringify(image));
 
-    fetch(URL + "/message/new", { 
+    fetch(URL + "/message/new", {
         method: "POST",
-        body: data})
-        .then(res => res.json())
-        .then(response => {
-            // TODO: React to response
-        });
+        body: data
+    }).then(response => {
+        // TODO: React to response
+    });
 
     console.log(image);
 }
@@ -237,12 +306,12 @@ function sendFile(file) {
     data.append("message_type", "File");
     data.append("content", JSON.stringify(file));
 
-    fetch(URL + "/message/new", { 
+    fetch(URL + "/message/new", {
         method: "POST",
-        body: data})
-        .then(res => res.json())
-        .then(response => {
-            // TODO: React to response
-        });
+        body: data
+    }).then(response => {
+        // TODO: React to response
+    });
+
     console.log(image);
 }
