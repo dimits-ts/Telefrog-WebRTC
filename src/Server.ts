a18f26d1b15ed42b1026efa511d411403cae554e
Anastasis575 <79604688+Anastasis575@users.noreply.github.com>
2022-12-23 14:23:07 +0200

    OK LISTEN HERE YOU LITTLE SHIT

--- src/Server.ts
+++ src/Server.ts
@@ -1,19 +1,19 @@
-import express, { Request, Response } from "express";
+import express, {Request, Response} from "express";
 import fs from "fs";
 import * as ht from "http";
 import s from "socket.io";
 import path from "path";
 import * as logging from "./logging";
-import { Message, Multimedia } from "./messages";
-import { constructMessage, flushUploads, getNewMessages, getUniqueRoomId, storeMessage } from "./routes";
-import multer, { FileFilterCallback } from "multer";
-import { randomUUID } from "crypto";
+import {Message, Multimedia} from "./messages";
+import {constructMessage, flushUploads, getNewMessages, getUniqueRoomId, storeMessage} from "./routes";
+import multer, {FileFilterCallback} from "multer";
+import {randomUUID} from "crypto";
 
 // Create the server
 const PORT = 8080;
 const app = express();
-var http = new ht.Server(app);
-var io = new s.Server(http, {
+const http = new ht.Server(app);
+const io = new s.Server(http, {
     maxHttpBufferSize: 10 * 1024 * 1024
 });
 
@@ -39,15 +39,15 @@ const upload = multer({
     }
 })
 
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json({limit: "10mb"}));
+app.use(express.urlencoded({extended: true, limit: "10mb"}));
 //Define where the static html content will be found.
 app.use("/static", express.static(path.join(__dirname, "../static")));
 app.use("/media", express.static(path.join(__dirname, "../uploads")));
 
 //This method redirects you to the html page when you enter localhost:8080
 
-var log: logging.Logging;
+let log: logging.Logging;
 // This is a file logger, if you want to change the path take into account that this will run from out/Server.js
 log = new logging.FileLog(path.join(__dirname, "../logging.txt"));
 //You can use the console version
@@ -68,7 +68,7 @@ app.get("/room/create", (req: Request, res: Response) => {
     chats.set(room, []);
     multimedia.set(room, []);
     log.i("Created room " + room);
-    res.status(200).send({ room });
+    res.status(200).send({room});
 })
 
 //Socket io connections
@@ -84,7 +84,7 @@ io.on("connection", socket => {
                 people.set(roomObj.room, person_count + 1)
                 socket.to(roomObj.room).emit("user-connected", roomObj.username, roomObj.peer);
                 socket.on("disconnect", () => {
-                    flushUploads(people,roomObj);
+                    flushUploads(people, roomObj);
                     socket.to(roomObj.room).emit("user-disconnected", roomObj.username, roomObj.peer);
                 });
                 socket.emit("join-status", 200, "OK");
@@ -94,32 +94,32 @@ io.on("connection", socket => {
 })
 
 //Callback to refresh the 
-app.get("/chat-box/refresh", (req: Request, res: Response) => {
+app.get("/chat-box/refresh", async (req: Request, res: Response) => {
     const room = String(req.query.roomId);
     const last_message = String(req.query.lastMessage);
 
-
-    getNewMessages(chats, room, last_message)
-        .then(toSend => res.status(200).json(toSend))
-        .catch(err => {
-            log.c(err.message);
-            res.sendStatus(400);
-        });
+    try {
+        let toSend = await getNewMessages(chats, room, last_message)
+        res.status(200).json(toSend)
+    } catch (e: any) {
+        log.c(e)
+    }
 });
 
 
 //Route for reading new Data
 app.post("/chat-box/message/new", upload.single("content"), (req: Request, res: Response) => {
+    let message;
     let roomId = req.body.roomId;
     if (req.body.messageType === "Text") {
-        var message = constructMessage(req.body.username, req.body.messageType, req.body.content);
-        storeMessage(roomId, res, message,chats,log);
+        message = constructMessage(req.body.username, req.body.messageType, req.body.content);
+        storeMessage(roomId, res, message, chats, log);
     } else {
         if (req.file === undefined) {
             res.status(400).send("File could not be uploaded");
         } else {
-            var message = constructMessage(req.body.username, req.body.messageType, req.file.filename);
-            storeMessage(roomId, res, message,chats,log);
+            message = constructMessage(req.body.username, req.body.messageType, req.file.filename);
+            storeMessage(roomId, res, message, chats, log);
         }
     }
 
@@ -130,7 +130,7 @@ fs.readdir(path.join(__dirname, "../uploads"), (err, files) => {
     if (!err) {
         for (const i of files) {
             if (fs.lstatSync(path.join(__dirname, "../uploads", i)).isDirectory()) {
-                fs.rmSync(path.join(__dirname, "../uploads", i), { recursive: true, force: true });
+                fs.rmSync(path.join(__dirname, "../uploads", i), {recursive: true, force: true});
             } else {
                 fs.unlinkSync(path.join(__dirname, "../uploads", i));
             }
