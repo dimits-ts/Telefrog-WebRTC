08ad21226887000cbbf85dfa7181ec65bce5a7ef
Anastasis575 <79604688+Anastasis575@users.noreply.github.com>
2023-01-08 16:03:50 +0200

    please just be fixxed

--- RunServer.bat
+++
@@ -1,6 +0,0 @@
-npm install
-
-npm run build
-
-start "Peer Server" npm run peer
-start "Web Server" npm start
--- RunServer.sh
+++
@@ -1,8 +0,0 @@
-trap "kill 0" EXIT
-npm --install
-
-npm run build
-
-npm run peer &
-npm start &
-wait
