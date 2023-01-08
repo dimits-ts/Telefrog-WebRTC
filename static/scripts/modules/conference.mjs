8d48cd48407440df68032b182484ac00d9b4b86c
Dimitris Tsirbas <manisfatsa@gmail.com>
2023-01-08 20:32:26 +0200

    frog.

--- static/scripts/modules/conference.mjs
+++ static/scripts/modules/conference.mjs
@@ -163,11 +163,8 @@ export class Conference {
      * @return the stream HTML element that was created, undefined if not created
      */
     #addVideoStream(username, stream, isMuted = false) {
-        if (username.length > 10) {
-            username = username.slice(0, 10) + "..."
-        }
+        // avoid bug with duplicate calls because of peer server call implementation
 
-        // avoid bug with duplicate calls because of peer server call impl
         if (stream.id !== this.#lastStreamId) {
             this.#lastStreamId = stream.id;
             return this.#presenter.addVideoElement(username, stream, isMuted);
